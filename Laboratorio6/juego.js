c30a37d349b9ed52afc98a2370c1e28f4c103566
Flavio Iglesias <profe.iglesias.sg@gmail.com>
2024-11-23 19:42:03 -0300

    Refinamiento

--- Laboratorio6/juego.js
+++ Laboratorio6/juego.js
@@ -1,15 +1,39 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
+let record = Infinity;
 
 function checkGuess() {
     const userGuess = parseInt(document.getElementById('guessInput').value);
+    const resultMessage = document.getElementById('resultMessage');
+    const attemptsDisplay = document.getElementById('attempts');
+    const recordDisplay = document.getElementById('record');
     attempts++;
 
     if (userGuess === randomNumber) {
-        document.getElementById('resultMessage').innerText = `¡Felicidades! Adivinaste el número en ${attempts} intentos.`;
+        resultMessage.innerText = `¡Felicidades! Adivinaste el número en ${attempts} intentos.`;
+        resultMessage.style.color = "gold";
+        if (attempts < record) {
+            record = attempts;
+            recordDisplay.innerText = `Récord: ${record}`;
+        }
+        resetGame();
     } else if (userGuess > randomNumber) {
-        document.getElementById('resultMessage').innerText = 'Intenta un número más bajo.';
+        resultMessage.innerText = 'Intenta un número más bajo.';
+        resultMessage.style.color = "Snow";
     } else {
-        document.getElementById('resultMessage').innerText = 'Intenta un número más alto.';
+        resultMessage.innerText = 'Intenta un número más alto.';
+        resultMessage.style.color = "Snow";
     }
+
+    attemptsDisplay.innerText = `Intentos: ${attempts}`;
+}
+
+function resetGame() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    attempts = 0;
+    setTimeout(() => {
+        document.getElementById('resultMessage').innerText = "";
+        document.getElementById('attempts').innerText = `Intentos: ${attempts}`;
+        document.getElementById('guessInput').value = "";
+    }, 3000);
 }
